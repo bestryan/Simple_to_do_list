11d1c24f3e6fdd496d49f4e32acb5170b942d4d3
bestryan88@gmail.com <bestryan88@gmail.com>
2023-03-25 10:42:53 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import NewTask from './Components/NewTask';
 import TasksList from './Components/TaskList';
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+
 
 
 function App() {
   const [newTask, setNewTask] = useState({});
+  
   const handleChange = ({ target }) => {
     const { name, value } = target;
     setNewTask((prev) => ({ ...prev, [name]: value, id: Date.now(), }));
   };
 
-  const [allTasks, setAllTasks] = useState([]);
+  const getLocalData = () => {
+    let data = localStorage.getItem('allTasks');
+    if (data) {
+      return JSON.parse(localStorage.getItem('allTasks'))
+    } else {
+      return [];
+    }
+  }
+
+  const [allTasks, setAllTasks] = useState(getLocalData());
+  
+  // add data to local storage
+  useEffect(() => {
+    localStorage.setItem('allTasks', JSON.stringify(allTasks));
+  }, [allTasks]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
